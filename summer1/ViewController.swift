ad988ae4db7a8a25d899cafec2a393be799b915d
Dung Pham <dungpham@Dungs-MacBook-Pro.local>
2017-05-03 22:38:43 -0400

    booleans

--- summer1/ViewController.swift
+++ summer1/ViewController.swift
@@ -17,27 +17,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var text2: UITextField!
     
-
+    
     
     @IBAction func Button(_ sender: Any) {
-    
-        print(text1.text!)
-        print(text2.text!)
-     Label.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+      let addition = true
+        if addition {
+            
+            Label.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        }else {
+            Label.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
     }
     
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
